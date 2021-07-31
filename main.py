b60522bfa8caa92bebd3bba98baee18cafcf7b18
Charlie Dixon <charlie-dixon@live.co.uk>
2021-07-31 17:22:34 +0100

    Dependency injection for database

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from sql_database import models
 from sqlalchemy.orm import Session
 from sql_database.database import SessionLocal, engine
 from pydantic import BaseModel
+from sql_database.models import Cryptocurrency
 
 app = FastAPI()
 
@@ -11,6 +12,25 @@ models.Base.metadata.create_all(bind=engine)
 class CryptoRequest(BaseModel):
     symbol: str
 
+def get_db():
+    """Creates database session - necessary for all API calls that require reading/writing to database"""
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.get("/")
 def home():
     return {"Home": "Page"}
+
+@app.post("/new_currency")
+def add_new_currency_to_db(crypto_request: CryptoRequest, db: Session = Depends(get_db)):
+    """Adds a user-defined cryptocurrency to the database."""
+    crypto = Cryptocurrency()
+    crypto.symbol = crypto_request.symbol
+
+    db.add(crypto)
+    db.commit()
+
+    return {f"{crypto.symbol}" : "Added"}

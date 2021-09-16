7c3434fed0ed76f488441ecbb8cb0d0ba5fcd986
Charlie Dixon <charlie-dixon@live.co.uk>
2021-09-16 17:31:34 +0100

    added func to set dropdown back to default text when closing modal or submitting form

--- static/user-interface.js
+++ static/user-interface.js
@@ -1,3 +1,9 @@
 function homepage() {
     window.location.href = "/"
   }
+
+
+function makeDefaultText() {
+    $("div.text", "#c2b").addClass("default")
+    $("div.text", "#c2b").html("Select coin to buy")
+}; 

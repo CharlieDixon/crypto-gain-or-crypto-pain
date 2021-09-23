33d1cd9d58a55ee3c373fba36a9cee690f01c856
Charlie Dixon <charlie-dixon@live.co.uk>
2021-09-23 12:26:02 +0100

    added in colour detection script for coins for use with analysis page graphs

---
+++ scripts/colour-detection.py
@@ -0,0 +1,27 @@
+"""Script which uses ColorThief package to find the dominant colour in a list of cryptocurrency png images and 
+then converts the resulting rgb value to a hex value for use in graph elements representing a given coin.
+Writes a json file containing these pairings."""
+from colorthief import ColorThief
+from os import listdir
+from os.path import isfile, join
+import json
+
+filelist = [f for f in listdir("pngs") if isfile(join("pngs", f))]
+
+
+def rgb_to_hex(rgb):
+    r, g, b = rgb
+    return ("{:X}{:X}{:X}").format(r, g, b)
+
+
+colours = {}
+for coin in filelist:
+    color_thief = ColorThief("pngs/" + coin)
+    dominant_colour = color_thief.get_color(quality=1)
+    hex_colour = rgb_to_hex(dominant_colour)
+    coin_name = coin.rstrip(".png").upper()
+    colours[coin_name] = hex_colour
+
+json_file = open("resources/crypto-colours.json", "w")
+json.dump(colours, json_file)
+json_file.close()
